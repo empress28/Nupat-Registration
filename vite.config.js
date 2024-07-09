6c8ac5b9a5435b41684d8f1c4b051bbbcd1ac188
empress28 <adamaigboempress@gmail.com>
2024-07-09 13:38:20 +0100

    made a change to the vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This line allows external access
+    port: 5173    // Optional: specify the port you want to use
+  }
 })
